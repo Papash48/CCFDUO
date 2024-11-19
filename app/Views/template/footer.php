cd1fea8e1dfb3e8ca079a0493833bd431d03aedd
Xuyky <xuyky91@gmail.com>
2024-11-19 16:24:56 +0100

    Update footer.php

--- app/Views/template/footer.php
+++ app/Views/template/footer.php
@@ -1,34 +1,26 @@
- <footer class="section novi-background footer-advanced bg-gray-700">
-        <div class="footer-advanced-main">
-          <div class="container">
-            <div class="row row-50">
-              <div class="col-lg-4">
-                <h5 class="font-weight-bold text-uppercase text-white">About Us</h5>
-                <p class="footer-advanced-text">inHouse is the largest full-service real estate and property management company. We offer expertise in the marketing and sale of a wide range of properties, including residential real estate, farms and lifestyle blocks, as well as commercial and industrial properties that we hope may interest you.</p>
-              </div>
-              <div class="col-sm-7 col-md-5 col-lg-4">
-                <h5 class="font-weight-bold text-uppercase text-white">Recent Blog Posts</h5>
-                <!-- Post Inline-->
-                <article class="post-inline">
-                  <p class="post-inline-title"><a href="#">Real Estate Guide: 7 Important Tips for Buying a Home</a></p>
-                  <ul class="post-inline-meta">
-                    <li>by Mike Barnes</li>
-                    <li><a href="#">2 days ago</a></li>
-                  </ul>
-                </article>
-                <!-- Post Inline-->
-                <article class="post-inline">
-                  <p class="post-inline-title"><a href="#">Single-Family Homes as a Housing Option for Young Families</a></p>
-                  <ul class="post-inline-meta"> 
-                    <li>by Mike Barnes</li>
-                    <li><a href="#">2 days ago</a></li>
-                  </ul>
-                </article>
-              </div>
-             
-              </div>
-            </div>
-          </div>
+<footer class="section novi-background footer-advanced bg-gray-700">
+<meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width height=device-height initial-scale=1.0 maximum-scale=1.0 user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Work+Sans:300,400,500,700,800%7CPoppins:300,400,700">
+    <link rel="stylesheet" href="public/css/bootstrap.css">
+    <link rel="stylesheet" href="public/css/fonts.css">
+    <link rel="stylesheet" href="public/css/style.css" id="main-styles-link">
+    <style>.ie-panel{display: none;background: #212121;padding: 10px 0;box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3);clear: both;text-align:center;position: relative;z-index: 1;} html.ie-10 .ie-panel, html.lt-ie-10 .ie-panel {display: block;}</style>
+  <div class="footer-advanced-main">
+    <div class="container">
+      <div class="row row-50">
+        <div class="col-lg-4">
+          <h5 class="font-weight-bold text-uppercase text-white">À propos de nous</h5>
+          <p class="footer-advanced-text">
+            Akor Adams Immobilier, fondée en 1950, est la plus grande agence immobilière et de gestion locative de la région. 
+            Nous proposons une expertise unique dans la vente et la location d'une large gamme de biens : résidences, propriétés agricoles, locaux commerciaux et industriels. 
+            Notre mission : concrétiser vos projets immobiliers avec professionnalisme et proximité.
+          </p>
         </div>
-        
-      </footer>
+      </div>
+    </div>
+  </div>
+</footer>
