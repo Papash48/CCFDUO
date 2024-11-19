be4a87d0054c56fa209c6e1ef02274b5e8345b33
Mathis DB <mathisdb48@gmail.com>
2024-11-19 17:24:31 +0100

    menu

--- app/Views/template/menu.php
+++ app/Views/template/menu.php
@@ -1,17 +1,35 @@
-    <div class="menu">
-        <ul>
-            <li class="logo">
-                <a href=""><?php echo img('public/img/title.png',true, 'alt="logo" class="logo"')?></a>
-            </li>
-            <li class="menu-toggle">
-                <button onclick="toggleMenu();">&#9776;</button>
-            </li>
-            <li class="menu-item hidden"><a href="">COMMANDES</a></li>
-            <li class="menu-item hidden"><?php echo anchor('recette/liste', 'A la carte') ?></li>
-            <li class="menu-item hidden"><?php echo anchor('ingredient/liste', 'Ingredients') ?></li>
-            <li class="menu-item hidden"><a href="">Reccueil</a></li>
-        </ul>
-    </div>
+<nav>
 
-</header>
+  <!-- Personnal space icon -->
+  <a href="/public/">
+    <aside class="pull-xs-right m-r-2" id="personal_space">
+      <i class="fa fa-user fa-3x m-r-1 hidden-xs-down" aria-hidden="true"></i>
+      <i class="fa fa-user fa-2x m-r-1 hidden-sm-up" id="small_user" aria-hidden="true"></i>
+      <span>Espace<br>personnel</span>
+    </aside>
+  </a>
+  <!-- Navigation -->
+  <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
+    &#9776;
+  </button>
+  <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">
+    <ul class="nav navbar-nav">
+      <li class="nav-item m-x-1">
+        <a class="nav-link" href="index.html">Accueil<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item m-x-1">
+        <a class="nav-link" href="achat.html">Achat</a>
+      </li>
+      <li class="nav-item m-x-1">
+        <a class="nav-link" href="location.html">Location</a>
+      </li>
+      <li class="nav-item m-x-1 on">
+        <a class="nav-link" href="presentation.html">Présentation</a>
+      </li>
+      <li class="nav-item m-x-1">
+        <a class="nav-link" href="agences.html">Nos agences</a>
+      </li>
+    </ul>
+  </div>
 
+</nav>
