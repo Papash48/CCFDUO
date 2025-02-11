f7bf043da556a074542cb541acdd4d812a5044bf
Xuyky <xuyky91@gmail.com>
2025-02-11 16:38:40 +0100

    a

--- app/Views/template/menu.php
+++ app/Views/template/menu.php
@@ -1,15 +1,15 @@
 <nav>
-  <!-- Personnal space icon -->
-  <a href="../connexion/compte">
-    <aside class="pull-xs-right m-r-2" id="personal_space">
-      <i class="fa fa-user fa-3x m-r-1 hidden-xs-down" aria-hidden="true"></i>
-      <i class="fa fa-user fa-2x m-r-1 hidden-sm-up" id="small_user" aria-hidden="true"></i>
-      <span>Espace personnel</span>
-    </aside>
-  </a>
-  
+    <?php
+    $lien = '<aside class="pull-xs-right m-r-2" id="personal_space">
+                  <i class="fa fa-user fa-3x m-r-1 hidden-xs-down" aria-hidden="true"></i>
+                  <i class="fa fa-user fa-2x m-r-1 hidden-sm-up" id="small_user" aria-hidden="true"></i>
+                  <span>Espace personnel</span>
+                </aside>';
+    echo anchor('connexion/compte',$lien); ?>
 
-  <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
+
+
+    <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
     &#9776;
   </button>
 
@@ -23,11 +23,11 @@
         <?php $session = session();
             if($session->type === 'agent'){
                 echo "<li class='nav-item m-x-1'>";
-                echo "<a class= 'nav-link ' href='../propriet/propriet_ajout'>Ajouter une propriété <span class='sr-only'>(current)</span></a>";
+                echo anchor('propriet/propriet_ajout','Ajouter une propriétée<span class="sr-only">(current)</span>','class="nav-link"');
                 echo "</li>";
             }?>
       <li class="nav-item m-x-1 on">
-        <a class="nav-link" href="../connexion/logout">Déconnexion</a>
+          <?php echo anchor('connexion/logout','Déconnexion<span class="sr-only">(current)</span>','class="nav-link"'); ?>
       </li>
     </ul> 
   </div>
--- app/Views/template/menu_acceuil.php
+++ app/Views/template/menu_acceuil.php
@@ -1,13 +1,11 @@
 <nav>
-    <!-- Personnal space icon -->
-    <a href="../connexion/compte">
-        <aside class="pull-xs-right m-r-2" id="personal_space">
-            <i class="fa fa-user fa-3x m-r-1 hidden-xs-down" aria-hidden="true"></i>
-            <i class="fa fa-user fa-2x m-r-1 hidden-sm-up" id="small_user" aria-hidden="true"></i>
-            <span>Espace personnel</span>
-        </aside>
-    </a>
-
+    <?php
+    $lien = '<aside class="pull-xs-right m-r-2" id="personal_space">
+                  <i class="fa fa-user fa-3x m-r-1 hidden-xs-down" aria-hidden="true"></i>
+                  <i class="fa fa-user fa-2x m-r-1 hidden-sm-up" id="small_user" aria-hidden="true"></i>
+                  <span>Espace personnel</span>
+                </aside>';
+    echo anchor('connexion/compte',$lien); ?>
 
     <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
         &#9776;
@@ -16,10 +14,10 @@
     <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2" style="background-color: #463f32;">
         <ul class="nav navbar-nav">
             <li class="nav-item m-x-1">
-                <a class="nav-link" href="./propriet/propriete">Accueil<span class="sr-only">(current)</span></a>
+                <?php echo anchor('propriet/propriete','Accueil<span class="sr-only">(current)</span>','class="nav-link"'); ?>
             </li>
             <li class="nav-item m-x-1 on">
-                <a class="nav-link" href="./connexion/logout">Déconnexion</a>
+                <?php echo anchor('connexion/logout','Déconnexion<span class="sr-only">(current)</span>','class="nav-link"'); ?>
             </li>
         </ul>
     </div>
